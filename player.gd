a0d0038c94038b616608014cdfe706496a2febf5
Toya Rosuello <victoria.rosuello@gmail.com>
2024-12-20 11:34:08 -0800

    logic for following mouse to look

--- player.gd
+++ player.gd
@@ -8,8 +8,27 @@ const JUMP_VELOCITY = 4.5
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 var interact_raycast
 
+var look_speed = 0.001
+var rotation_x = 0.0
+var rotation_y = 0.0
+
 func _ready() -> void:
 	interact_raycast = get_child(1)
+
+func _input(event):
+	if (event is InputEventMouseMotion):
+		var mouse_motion = event as InputEventMouseMotion
+		rotation_x = mouse_motion.relative.x * look_speed
+		rotation_y = mouse_motion.relative.y * look_speed
+		
+		var temp_transform = transform
+		transform.basis = Basis.IDENTITY
+		transform = temp_transform
+
+		var clamped_y = clamp(rotation_y, deg_to_rad(-90), deg_to_rad(90))
+
+		rotate_object_local(Vector3.UP, rotation_x * -1)
+		rotate_object_local(Vector3.LEFT, clamped_y)
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -25,8 +44,6 @@ func _physics_process(delta):
 			if interact.has_method("door_interact_emitter"):
 				interact.door_interact_emitter()
 	
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:

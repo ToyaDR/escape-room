d36b7126e7bb78bc810bdf011c9a8f06dded22b3
nessaro <48305726+nessaro@users.noreply.github.com>
2025-04-24 10:52:52 -0700

    Bedroom blocking (#11)
    * added carpet textures and blocked in bedroom assets
    
    * Door physics prototype v3 (#10)
    
    * Fix door physics (while nothing is carried)
    
    * Emit signal to freeze doors
    
    * added furniture assets and ceiling fan
    
    * made a triplanar BOTW shader
    
    * botw triplanar texture and bunkbed
    
    ---------
    
    Co-authored-by: ToyaDR <ToyaDR@users.noreply.github.com>

--- doorInteract.gd
+++ doorInteract.gd
@@ -1,12 +1,23 @@
-extends StaticBody3D
+extends RigidBody3D
 
 signal door_interact
+var door_opener: Marker3D
 
+var update_inventory = false
 func interact() -> void:
-	print("door hit")
-	door_interact.emit()
+	var door_opener_position = door_opener.transform.origin;
+	var door_opener_impulse = Vector3(0, 0, 2.0)
+	# print("\n\nPOSITION: ("
+	# + str(door_opener_position.x) + ","
+	# + str(door_opener_position.y) + ","
+	# + str(door_opener_position.z) + ") \nFORCE: ("
+	# + str(door_opener_impulse.x) + ","
+	# + str(door_opener_impulse.y) + ","
+	# + str(door_opener_impulse.z) + ")");
+	apply_impulse(door_opener_impulse, door_opener_position);
 
 func _ready() -> void:
+	door_opener = get_node("opener");
 	pass
 
 func _process(_delta: float) -> void:
